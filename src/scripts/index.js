3e1099b02fbf2d91d9ea1e7791cc64aa1b72a9ed
Rasmus Thirslund <rasmus.thirslund@implicit.dk>
2018-09-11 09:07:10 +0200

    site footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,49 @@
-import '../styles/index.scss'; // needed to compile sass into a css file
+import '../scss/index.scss'; // needed to compile sass into a css file
 
 console.log('this!');
+
+let sel = '.js-collapse';
+
+if (document.querySelectorAll(sel).length) {
+
+    let aCollapsers = document.querySelectorAll(sel);
+    const CLASS_TOGGLER = '.js-collapse-trigger',
+        CLASS_TARGET = '.js-collapse-target';
+
+    console.log(aCollapsers);
+
+    for (let section of aCollapsers) {
+
+        let s = new Collapser(section);
+
+
+        // console.log(section);
+        // let toggler = section.getElementsByClassName(CLASS_TRIGGER);
+        // toggler.addEventListener('click', function(e){
+        //     console.log(e);
+        // });
+
+    }
+    ;
+
+    function Collapser(section) {
+
+        //this.trigger
+        //
+        let toggler = section.querySelector(CLASS_TOGGLER);
+        let target = section.querySelector(CLASS_TARGET);
+
+        if (toggler) {
+            console.log('inside collapser', toggler, toggler.classList, toggler.className);
+
+            toggler.addEventListener('click', function (e) {
+                console.log(this, e);
+                section.classList.toggle('is-active');
+            });
+        }
+
+
+    }
+
+
+}
